90d72162e43b2eff50a78b4e1c8d244b0fad0abe
NguynCa <phuongthaopham116@gmail.com>
2022-12-20 22:45:31 +0700

    update: code multiply matrices

--- PrjWeek11/MulMatrixVector/Program.cs
+++ PrjWeek11/MulMatrixVector/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace MulMatrixVector
 {
@@ -13,26 +15,38 @@ namespace MulMatrixVector
             Calculation.ProcessInitialization(ref Calculation.pVector, Calculation.size);
 
             //multithread
-            DateTime startTime = DateTime.Now;
+            //DateTime startTime = DateTime.Now;
             Console.WriteLine("Start Multithread Code");
+            Console.Error.WriteLine("Executing sequential loop...");
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
             Calculation.ParallelResultCalculation();
             //Calculation.PrintVector(Calculation.pResult, Calculation.size);
             Console.WriteLine("End Multithread Code!!!");
-            DateTime endTime = DateTime.Now;
-            TimeSpan Duration1 = endTime - startTime;
+            stopwatch.Stop();
+            Console.Error.WriteLine("Sequential loop time in milliseconds: {0}",
+                                    stopwatch.ElapsedMilliseconds);
+            //DateTime endTime = DateTime.Now;
+            //TimeSpan Duration1 = endTime - startTime;
+
+            stopwatch.Reset();
 
             Calculation.ProcessInitialization(ref Calculation.pVector, Calculation.size);
             //sequence
-            startTime = DateTime.Now;
+            //startTime = DateTime.Now;
             Console.WriteLine("Start Singlethread Code");
+            stopwatch.Start();
             Calculation.SerialResultCalculation(Calculation.pMatrix, Calculation.pVector, ref Calculation.pResult, Calculation.size);
             //Calculation.PrintVector(Calculation.pResult, Calculation.size);
             Console.WriteLine("End Singlethread Code!!!");
-            endTime = DateTime.Now;
-            TimeSpan Duration2 = endTime - startTime;
+            stopwatch.Stop();
+            Console.Error.WriteLine("Parallel loop time in milliseconds: {0}",
+                                    stopwatch.ElapsedMilliseconds);
+            //endTime = DateTime.Now;
+            //TimeSpan Duration2 = endTime - startTime;
 
-            Console.WriteLine($"\nTime of Multithread Code: {Duration1.TotalMilliseconds,10}"
-                + $"\nTime of Sequence Code:    {Duration2.TotalMilliseconds,10}");
+            //Console.WriteLine($"\nTime of Multithread Code: {Duration1.TotalMilliseconds,10}"
+            //    + $"\nTime of Sequence Code:    {Duration2.TotalMilliseconds,10}");
             Console.ReadKey();
         }
     }
---
+++ PrjWeek11/MultiplyMatrices/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace MultiplyMatrices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Set up matrices. Use small values to better view
+            // result matrix. Increase the counts to see greater
+            // speedup in the parallel loop vs. the sequential loop.
+            int colCount = 180;
+            int rowCount = 5000;
+            int colCount2 = 270;
+            double[,] m1 = Calculation.InitializeMatrix(rowCount, colCount);
+            double[,] m2 = Calculation.InitializeMatrix(colCount, colCount2);
+            double[,] result = new double[rowCount, colCount2];
+
+            // First do the sequential version.
+            Console.Error.WriteLine("Executing sequential loop...");
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            Calculation.MultiplyMatricesSequential(m1, m2, result);
+            stopwatch.Stop();
+            Console.Error.WriteLine("Sequential loop time in milliseconds: {0}",
+                                    stopwatch.ElapsedMilliseconds);
+
+            // For the skeptics.
+            Calculation.OfferToPrint(rowCount, colCount2, result);
+
+            // Reset timer and results matrix.
+            stopwatch.Reset();
+            result = new double[rowCount, colCount2];
+
+            // Do the parallel loop.
+            Console.Error.WriteLine("Executing parallel loop...");
+            stopwatch.Start();
+            Calculation.MultiplyMatricesParallel(m1, m2, result);
+            stopwatch.Stop();
+            Console.Error.WriteLine("Parallel loop time in milliseconds: {0}",
+                                    stopwatch.ElapsedMilliseconds);
+            Calculation.OfferToPrint(rowCount, colCount2, result);
+
+            // Keep the console window open in debug mode.
+            Console.Error.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
