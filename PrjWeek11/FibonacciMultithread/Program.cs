040016dcd1f26aa94dbd45aa220d6ca307b0f2e1
NguynCa <phuongthaopham116@gmail.com>
2022-12-20 18:02:58 +0700

    update: FibonacciSequence

--- PrjWeek11/FibonacciMultithread/Program.cs
+++ PrjWeek11/FibonacciMultithread/Program.cs
@@ -5,38 +5,38 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace FibonacciMultithread
+namespace Fibonacci
 {
     class Program
     {
-        static int[] a = new int[30];
-        static int Fibonacci (int n)
-        {
-            double x = 1.0 / Math.Sqrt(5.0);
-            double y = (1.0 + Math.Sqrt(5.0)) / 2.0;
-            double z = (1.0 - Math.Sqrt(5.0)) / 2.0;
-            return (int)Math.Round(x * (Math.Pow(y, n) - Math.Pow(z, n)));
-        }
-        static void PrintFibo (int n)
-        {
-            //Console.WriteLine($"task:{Task.CurrentId,3}  " + $"thread: {Thread.CurrentThread.ManagedThreadId}");
-            a[n] = Fibonacci(n);
-            //Console.WriteLine($"{fiboNumber,10}");
-            //Thread.Sleep(1000);
-        }
-        public static void ParallelFor()
-        {
-            ParallelLoopResult result = Parallel.For(0, 29, PrintFibo);
-            Console.WriteLine($"All task start and finish: {result.IsCompleted}");
-            //Console.WriteLine(result);
-        }
         static void Main(string[] args)
         {
-            ParallelFor();
-            foreach (int i in a)
+            Console.Write("Enter the number of Fibonacci digits: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            //multithread
+            DateTime startTime = DateTime.Now;
+            Console.WriteLine("Start Multithread Code: ");
+            FibonacciMultithread.ParallelFor(n);
+            for (int i = 0; i < n; i++)
             {
-                Console.Write(i + " ");
+                Console.Write(FibonacciMultithread.a[i] + " ");
             }
+            Console.WriteLine("\nEnd Multithread Code!!!");
+            DateTime endTime = DateTime.Now;
+            TimeSpan Duration1 = endTime - startTime;
+
+            //sequence
+            startTime = DateTime.Now;
+            Console.WriteLine("Start Singlethread Code: ");
+            FibonacciSequence.PrintFibo(n);
+            Console.WriteLine("\nEnd Singlethread Code!!!");
+            endTime = DateTime.Now;
+            TimeSpan Duration2 = endTime - startTime;
+
+            Console.WriteLine($"\nTime of Multithread Code: {Duration1.TotalMilliseconds,10}"
+                + $"\nTime of Sequence Code:    {Duration2.TotalMilliseconds,10}");
+
             Console.ReadKey();
         }
     }
